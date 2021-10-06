4a7b04032f75c91469bb5e776e1f79a576a85559
Morgan Rockett <morgan.rockett@tufts.edu>
2021-10-06 16:43:55 -0400

    part 1 filegen and output done working

--- hw2_caches.py
+++ hw2_caches.py
@@ -7,50 +7,50 @@ import math
 import argparse
 import subprocess
 import re
-
-global sample_cache
-sample_cache = """
-# Sample configuration for SRAM cache
-
--DesignTarget: cache
-
--CacheAccessMode: Sequential
-
--OptimizationTarget: ReadEDP
-
--EnablePruning: Yes
-
--ProcessNode: 22
-
--Capacity(KB): 256
--WordWidth(bit): 512
--Associativity(
-for cache only): 16
-
--DeviceRoadmap: LSTP
-
--LocalWireType: LocalAggressive
--LocalWireRepeaterType: RepeatedNone
-
--LocalWireUseLowSwing: No
-
--GlobalWireType: GlobalAggressive
--GlobalWireRepeaterType: RepeatedNone
-
--GlobalWireUseLowSwing: No
-
--Routing: H - tree
-
--InternalSensing: true
-
--MemoryCellInputFile:./ cell_defs / SRAM.cell
-
--Temperature(K): 350
-
--BufferDesignOptimization: latency
-
--UseCactiAssumption: Yes
-"""
+#
+# global sample_cache
+# sample_cache = """
+# # Sample configuration for SRAM cache
+#
+# -DesignTarget: cache
+#
+# -CacheAccessMode: Sequential
+#
+# -OptimizationTarget: ReadEDP
+#
+# -EnablePruning: Yes
+#
+# -ProcessNode: 22
+#
+# -Capacity(KB): 256
+# -WordWidth(bit): 512
+# -Associativity(
+# for cache only): 16
+#
+# -DeviceRoadmap: LSTP
+#
+# -LocalWireType: LocalAggressive
+# -LocalWireRepeaterType: RepeatedNone
+#
+# -LocalWireUseLowSwing: No
+#
+# -GlobalWireType: GlobalAggressive
+# -GlobalWireRepeaterType: RepeatedNone
+#
+# -GlobalWireUseLowSwing: No
+#
+# -Routing: H - tree
+#
+# -InternalSensing: true
+#
+# -MemoryCellInputFile:./ cell_defs / SRAM.cell
+#
+# -Temperature(K): 350
+#
+# -BufferDesignOptimization: latency
+#
+# -UseCactiAssumption: Yes
+# """
 
 
 """ Run
@@ -93,10 +93,27 @@ def path_test(input_path):
     if not os.path.exists(input_path):
         print(f'path not found: {input_path}\n')
         sys.exit(-1)
+
+
+def pretty_time(t0: float, t1: float):
+    # t0 = time.perf_counter()  # call in script
+    # t1 = time.perf_counter()  # call in script
+    time_delta: float = t1 - t0
+    if time_delta < 60:
+        msg = f'\nelapsed time: {round(time_delta, 3)} seconds\n'
+    else:
+        mins = int(math.floor(time_delta / 60))
+        secs = int(math.floor(time_delta % 60))
+        msg = f'\nelapsed time: {mins} min {secs} secs\n'
+
+    print(msg)
+    return msg
 # helper functions
 
 
 def create_cache_files():
+
+    sample_cache = os.path.join(cache_path, "SRAM_cache.cfg")
 
     for cap in [16, 32, 64, 128, 256, 512, 1024, 2048]:  # 16kB to 2MB cache size
 
@@ -106,18 +123,22 @@ def create_cache_files():
             output_file = os.path.join(cache_path, gen_file)
             with open(output_file, 'w') as fo:
 
-                for line in sample_cache:
-                    result_cap = re.match(r"(-Capacity \(KB\): )(\d+)(\s*)", line)
-                    result_assoc = re.match(r"(-Associativity (for cache only): )(\d+)(\s*)", line)
+                with open(sample_cache, 'r') as fi:
+                    for line in fi: # sample_cache
+                    
+                        result_cap = re.match(r"(-Capacity\s\(KB\): )(\d+)(\s*)", line)
+                        result_assoc = re.match(r"(-Associativity\s\(for cache only\): )(\d+)(\s*)", line)
 
-                    if result_cap:
-                        capacity = result_cap.group(1) + str(cap) + result_cap.group(3)
-                        fo.write(capacity)
-                    elif result_assoc:
-                        associativity = result_assoc.group(1) + str(assoc) + result_assoc.group(3)
-                        fo.write(associativity)
-                    else:
-                        fo.write(line)
+                        if result_cap:
+                            capacity = result_cap.group(1) + str(cap) + result_cap.group(3)
+                            fo.write(capacity)
+                        elif result_assoc:
+                            associativity = result_assoc.group(1) + str(assoc) + result_assoc.group(3)
+                            fo.write(associativity)
+                        else:
+                            fo.write(line)
+
+                fi.close()
 
             fo.close()
 
@@ -125,11 +146,11 @@ def create_cache_files():
 def run_nvsim():
 
     nvsim_path = os.path.realpath("nvsim")
-    print("\nnvsim_path", nvsim_path)
+    # print("\nnvsim_path:\n\t", nvsim_path)
     path_test(nvsim_path)
 
     for root, dirs, files in os.walk(cache_path):
-        print("root", root)
+        # print("root", root)
         for file in files: # cache file
             if re.search(r".cfg", file): # cfg files only
 
@@ -138,15 +159,25 @@ def run_nvsim():
 
                 full_file = os.path.join(cache_path, file)
 
-                cmd = nvsim_path + " " + full_file + " > " + output_file
-                print("\nnvsim cmd", cmd)
-                shell_exec(cmd)
+                try:
+                    # cmd = nvsim_path + " " + full_file + " > " + output_file
+                    cmd = f'{nvsim_path} {full_file} > {output_file}'
+                    # cmd = f'{nvsim_path} {full_file}'
+                    print("\nnvsim cmd:\n\t", cmd)
+                    stdout, stderr, exit_code = shell_exec(cmd)
 
-                print("\ncache results: ", output_file)
-
-
+                    print("\ncache results:\n\t", output_file)
+                    # print("\ncache results:\n\t", stdout)
+                    time.sleep(8)
+                    # break
+                except:
+                    print("\ncouldn't execute nv sim cmd\n")
+            else:
+                pass
 
 if __name__ == "__main__":
+
+    t0 = time.perf_counter()
 
     args = cli_parse()
 
@@ -155,3 +186,7 @@ if __name__ == "__main__":
     create_cache_files()
 
     run_nvsim()
+
+    t1 = time.perf_counter()
+
+    pretty_time(t0, t1)
